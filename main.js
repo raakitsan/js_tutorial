d227ac99c3832619d96e90253ca42405ec605d10
raakitsan <brian.raak@gmail.com>
2022-06-21 05:10:35 -0500

    did both

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function palindromeTester() {
   if (phrase.palindrome()) {
     palindromeResult.innerHTML = `<strong>"${phrase.content}"</strong>` + "is a palindrome!";
   } else {
-    palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome.`;
+    palindromeResult.innerHTML = `<strong>"${phrase.content}"</strong>` + "is not a palindrome!";
   }
 }
 
